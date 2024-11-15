412d214dac6851c41383361f5fe931baa43c81ce
ZqinKing <zqinking23@gmail.com>
2024-11-15 13:01:52 +0000

    test code-server

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ make download -j$(nproc)
 make -j$(nproc) || make -j1 || make -j1 V=s
 
 FIRMWARE_DIR="$BASE_PATH/firmware"
-\rm -rf "$FIRMWARE_DIR"
+rm -rf "$FIRMWARE_DIR"
 mkdir -p "$FIRMWARE_DIR"
 find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
-\rm -f "$BASE_PATH/firmware/Packages.manifest" 2>/dev/null
+rm -f "$BASE_PATH/firmware/Packages.manifest" 2>/dev/null
